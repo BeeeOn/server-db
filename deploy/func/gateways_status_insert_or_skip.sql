75b4a28f73e5e319b317d470e3b10410ff13cfb6
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-07-10 02:29:43 +0200

    beeeon: implement gateways_status_insert_or_skip

---
+++ deploy/func/gateways_status_insert_or_skip.sql
@@ -0,0 +1,39 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION beeeon.gateways_status_insert_or_skip(
+	_gateway_id bigint,
+	_at bigint,
+	_version varchar(40),
+	_ip varchar(45)
+)
+RETURNS VOID AS
+$$
+DECLARE
+	count bigint;
+BEGIN
+	SELECT COUNT(*) FROM beeeon.gateways_status
+	INTO count
+	WHERE
+		gateway_id = _gateway_id
+		AND
+		version = _version
+		AND
+		ip = _ip::inet
+		AND
+		at = beeeon.gateways_status_most_recent(_gateway_id);
+	
+	IF count = 0 THEN
+		INSERT INTO beeeon.gateways_status (gateway_id, at, version, ip)
+		VALUES (
+			_gateway_id,
+			to_timestamp(_at),
+			_version,
+			_ip::inet
+		);
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+COMMIT;

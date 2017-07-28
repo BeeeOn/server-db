BeeeOn/server-db

commit 1e343ac2a603d55f033f10037caad22362d12007

beeeon: implement devices_active_by_idrange_and_gateway

deploy/func/devices_active_by_idrange_and_gateway.sql
@@ -1,0 +1,38 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION beeeon.devices_active_by_idrange_and_gateway(
+	_gateway_id bigint,
+	_min_id numeric,
+	_max_id numeric
+)
+RETURNS SETOF beeeon.device AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT
+			beeeon.from_device_id(id),
+			gateway_id,
+			location_id,
+			name,
+			type,
+			refresh,
+			battery,
+			signal,
+			extract(epoch FROM first_seen)::bigint,
+			extract(epoch FROM last_seen)::bigint,
+			extract(epoch FROM active_since)::bigint
+		FROM beeeon.devices
+		WHERE
+			id >= beeeon.to_device_id(_min_id)
+			AND
+			id <= beeeon.to_device_id(_max_id)
+			AND
+			active_since IS NOT NULL
+			AND
+			gateway_id = _gateway_id;
+END;
+$$ LANGUAGE plpgsql;
+
+COMMIT;

revert/func/devices_active_by_idrange_and_gateway.sql
@@ -1,0 +1,7 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+DROP FUNCTION beeeon.devices_active_by_idrange_and_gateway(bigint, numeric, numeric);
+
+COMMIT;

verify/func/devices_active_by_idrange_and_gateway.sql
@@ -1,0 +1,10 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'devices_active_by_idrange_and_gateway(bigint, numeric, numeric)'
+);
+
+ROLLBACK;

sqitch.plan
@@ -68,3 +68,4 @@ func/devices_insert [schema-beeeon type/device table/devices] 2017-07-14T08:48:4
 func/devices_update [schema-beeeon type/device table/devices] 2017-07-14T11:08:19Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement devices_update
 func/devices_active_by_gateway [schema-beeeon type/device table/devices] 2017-07-14T11:33:34Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement devices_active_by_gateway
 func/devices_inactive_by_gateway [schema-beeeon type/device table/devices] 2017-07-14T11:40:45Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement devices_inactive_by_gateway
+func/devices_active_by_idrange_and_gateway [schema-beeeon type/device table/devices] 2017-07-14T15:45:12Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement devices_active_by_idrange_and_gateway

test/t4085-devices_active_by_idrange_and_gateway.sql
@@ -1,0 +1,19 @@
+SET client_min_messages TO warning;
+CREATE EXTENSION IF NOT EXISTS pgtap;
+RESET client_min_messages;
+
+SET search_path TO beeeon, public;
+
+BEGIN;
+
+SELECT plan(2);
+
+SELECT has_function('devices_active_by_idrange_and_gateway');
+
+SELECT is_empty(
+	$$ SELECT * FROM devices_active_by_idrange_and_gateway(1157178494608281, 11673330234144325632, 11745387828182253567) $$,
+	'there is nothing yet in the devices table'
+);
+
+SELECT finish();
+ROLLBACK;

commit 112eb1a530f95a681283186c89699409d91200de

func: fix devices_active_by_idrange_and_gateway bad param types
Fixes: 1e343ac2a603 ("beeeon: implement devices_active_by_idrange_and_gateway")

Signed-off-by: Jozef Halaj <xhalaj03@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

deploy/func/devices_active_by_idrange_and_gateway.sql
@@ -4,8 +4,8 @@ BEGIN;
 
 CREATE OR REPLACE FUNCTION beeeon.devices_active_by_idrange_and_gateway(
 	_gateway_id bigint,
-	_min_id numeric,
-	_max_id numeric
+	_min_id numeric(20, 0),
+	_max_id numeric(20, 0)
 )
 RETURNS SETOF beeeon.device AS
 $$
a3dd34038adb20d7e9444e8ec0304fc86813046f
Jan Viktorin <iviktorin@fit.vutbr.cz>
2017-10-03 16:24:51 +0200

    beeeon: implement controls_recent_state
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- test/t4121-controls_recent_state.sql
+++ test/t4121-controls_recent_state.sql
@@ -6,7 +6,7 @@ SET search_path TO beeeon, public;
 
 BEGIN;
 
-SELECT plan(2);
+SELECT plan(6);
 
 SELECT has_function('controls_recent_state');
 
@@ -20,49 +20,205 @@ SELECT ok(
 INSERT INTO users (id, first_name, last_name, locale)
 VALUES ('2bcf60a9-cd37-4f27-a7ab-3e6b740770ed', 'Joe', 'Doe', 'en');
 
-INSERT INTO controls_fsm_history (id, at, value, stability, originator_user_id)
+INSERT INTO gateways (id, name)
+VALUES (1240795450208837, 'testing gateway');
+
+INSERT INTO devices (id, gateway_id, name, type, first_seen, last_seen)
 VALUES
 (
-	'56a18328-e326-45ff-8544-c9503babb34a',
-	to_timestamp(1507036096) at time zone 'UTC',
+	to_device_id(11678152912333531136::numeric(20, 0)),
+	1240795450208837,
+	'testing device',
+	0,
+	timestamp with time zone '2017-10-1 8:00:00',
+	timestamp with time zone '2017-10-4 9:00:00'
+);
+
+INSERT INTO controls_recent (gateway_id, device_id, module_id, value, at, stability, originator_user_id)
+VALUES
+(
+	1240795450208837,
+	to_device_id(11678152912333531136::numeric(20, 0)),
+	0,
 	5::real,
+	timestamp with time zone '2017-10-2 15:00:00',
 	'requested',
 	'2bcf60a9-cd37-4f27-a7ab-3e6b740770ed'
 ),
 (
-	'5b0d4c2f-5995-4120-8adf-d4d829091220',
-	to_timestamp(1507036253) at time zone 'UTC',
+	1240795450208837,
+	to_device_id(11678152912333531136::numeric(20, 0)),
+	0,
 	5::real,
+	timestamp with time zone '2017-10-2 15:01:04',
 	'accepted',
 	NULL
-),
+);
+
+---
+-- The control 0 was requested to change its value. The change would probably
+-- occur well as we've already received acknowladge - thus 'accepted' state.
+-- There is however no confirmed value yet.
+---
+SELECT results_eq(
+	$$ SELECT
+		to_timestamp(confirmed_state_at),
+		confirmed_state_value,
+		confirmed_state_stability,
+		confirmed_state_originator_user_id,
+		to_timestamp(current_state_at),
+		current_state_value,
+		current_state_stability,
+		current_state_originator_user_id
+	FROM controls_recent_state(
+		1240795450208837,
+		11678152912333531136::numeric(20, 0),
+		0::smallint
+	) $$,
+	$$ VALUES (
+		NULL::timestamp with time zone,
+		NULL::real,
+		NULL::smallint,
+		NULL::uuid,
+		timestamp with time zone '2017-10-2 15:01:04',
+		5::real,
+		from_control_stability('accepted'),
+		NULL::uuid
+	) $$,
+	'expected no confirmed state and the current state to be "accepted"'
+);
+
+INSERT INTO controls_recent (gateway_id, device_id, module_id, value, at, stability, originator_user_id)
+VALUES
 (
-	'3c7a7bdc-300a-4d5a-bc4a-f8d90afe70ff',
-	to_timestamp(1507036407) at time zone 'UTC',
+	1240795450208837,
+	to_device_id(11678152912333531136::numeric(20, 0)),
+	0,
 	5::real,
+	timestamp with time zone '2017-10-2 15:01:46',
 	'confirmed',
 	NULL
-),
+);
+
+---
+-- The requested value for control 0 has been confirmed.
+---
+SELECT results_eq(
+	$$ SELECT
+		to_timestamp(confirmed_state_at),
+		confirmed_state_value,
+		confirmed_state_stability,
+		confirmed_state_originator_user_id,
+		to_timestamp(current_state_at),
+		current_state_value,
+		current_state_stability,
+		current_state_originator_user_id
+	FROM controls_recent_state(
+		1240795450208837,
+		11678152912333531136::numeric(20, 0),
+		0::smallint
+	) $$,
+	$$ VALUES (
+		timestamp with time zone '2017-10-2 15:01:46',
+		5::real,
+		from_control_stability('confirmed'),
+		NULL::uuid,
+		timestamp with time zone '2017-10-2 15:01:46',
+		5::real,
+		from_control_stability('confirmed'),
+		NULL::uuid
+	) $$,
+	'expected the confirmed state and the current state to be equal'
+);
+
+INSERT INTO controls_recent (gateway_id, device_id, module_id, value, at, stability, originator_user_id)
+VALUES
 (
-	'f3f1bc0b-421d-4935-9226-1abe45c22f42',
-	to_timestamp(1507036431) at time zone 'UTC',
+	1240795450208837,
+	to_device_id(11678152912333531136::numeric(20, 0)),
+	0,
 	15::real,
+	timestamp with time zone '2017-10-2 15:05:11',
 	'requested',
 	'2bcf60a9-cd37-4f27-a7ab-3e6b740770ed'
-),
+);
+
+---
+-- The user has requested for another change of control 0. Thus,
+-- we now have a stable/confirmed state and a change in progress.
+---
+SELECT results_eq(
+	$$ SELECT
+		to_timestamp(confirmed_state_at),
+		confirmed_state_value,
+		confirmed_state_stability,
+		confirmed_state_originator_user_id,
+		to_timestamp(current_state_at),
+		current_state_value,
+		current_state_stability,
+		current_state_originator_user_id
+	FROM controls_recent_state(
+		1240795450208837,
+		11678152912333531136::numeric(20, 0),
+		0::smallint
+	) $$,
+	$$ VALUES (
+		timestamp with time zone '2017-10-2 15:01:46',
+		5::real,
+		from_control_stability('confirmed'),
+		NULL::uuid,
+		timestamp with time zone '2017-10-2 15:05:11',
+		15::real,
+		from_control_stability('requested'),
+		'2bcf60a9-cd37-4f27-a7ab-3e6b740770ed'::uuid
+	) $$,
+	'expected an existing confirmed state and the current state to be "requested"'
+);
+
+INSERT INTO controls_recent (gateway_id, device_id, module_id, value, at, stability, originator_user_id)
+VALUES
 (
-	'fd179e29-51ea-4bec-8849-49e5f939ff70',
-	to_timestamp(1507036457) at time zone 'UTC',
+	1240795450208837,
+	to_device_id(11678152912333531136::numeric(20, 0)),
+	0,
 	8::real,
+	timestamp with time zone '2017-10-2 15:05:11',
 	'overriden',
 	NULL
 );
 
-INSERT INTO controls_recent (id, gateway_id, device_id, module_id, confirmed_state_id, current_state_id)
-VALUES (
-	'faa6004d-8b06-44b8-9d25-9f7bcad53773'
-	'3c7a7bdc-300a-4d5a-bc4a-f8d90afe70ff',
-	'f3f1bc0b-421d-4935-9226-1abe45c22f42'
+---
+-- While requesting (and waiting) a new change of control 0, the control 0
+-- reports another value. This does not mean, the request is cancelled because
+-- both the user and the control has sent the message at the same time.
+-- However, the last stable (confirmed) value is now 8, the 'overriden' value.
+---
+SELECT results_eq(
+	$$ SELECT
+		to_timestamp(confirmed_state_at),
+		confirmed_state_value,
+		confirmed_state_stability,
+		confirmed_state_originator_user_id,
+		to_timestamp(current_state_at),
+		current_state_value,
+		current_state_stability,
+		current_state_originator_user_id
+	FROM controls_recent_state(
+		1240795450208837,
+		11678152912333531136::numeric(20, 0),
+		0::smallint
+	) $$,
+	$$ VALUES (
+		timestamp with time zone '2017-10-2 15:05:11',
+		8::real,
+		from_control_stability('overriden'),
+		NULL::uuid,
+		timestamp with time zone '2017-10-2 15:05:11',
+		15::real,
+		from_control_stability('requested'),
+		'2bcf60a9-cd37-4f27-a7ab-3e6b740770ed'::uuid
+	) $$,
+	'expected the confirmed state to be "overriden" and the current state to be "requested"'
 );
 
 SELECT finish();

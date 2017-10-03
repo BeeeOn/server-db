BeeeOn/server-db

commit 0873ff60d3f4a4f1be0baf70ba88db3f71fe171e

beeeon: implement device_properties_by_device

deploy/func/device_properties_by_device.sql
@@ -1,0 +1,27 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION beeeon.device_properties_by_device(
+	_device_id numeric,
+	_gateway_id bigint
+)
+RETURNS SETOF beeeon.device_property AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT
+			beeeon.from_device_id(device_id),
+			gateway_id,
+			key,
+			value,
+			params
+		FROM beeeon.device_properties
+		WHERE
+			device_id = _device_id
+			AND
+			gateway_id = _gateway_id;
+END;
+$$ LANGUAGE plpgsql;
+
+COMMIT;

revert/func/device_properties_by_device.sql
@@ -1,0 +1,7 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+DROP FUNCTION beeeon.device_properties_by_device(numeric, bigint);
+
+COMMIT;

verify/func/device_properties_by_device.sql
@@ -1,0 +1,10 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'device_properties_by_device(numeric, bigint)'
+);
+
+ROLLBACK;

sqitch.plan
@@ -73,3 +73,4 @@ func/devices_by_id_and_gateway [schema-beeeon type/device table/devices] 2017-07
 func/device_properties_insert [schema-beeeon type/device_property type/device table/device_properties] 2017-07-14T12:22:51Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement device_properties_insert
 func/device_properties_update [schema-beeeon type/device_property type/device table/device_properties] 2017-07-14T12:26:19Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement device_properties_update
 func/device_properties_by_key [schema-beeeon type/device_property type/device table/device_properties] 2017-07-14T12:30:11Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement device_properties_by_key
+func/device_properties_by_device [schema-beeeon type/device_property type/device table/device_properties] 2017-07-14T13:15:20Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement device_properties_by_device

test/t4094-device_properties_by_device.sql
@@ -1,0 +1,19 @@
+SET client_min_messages TO warning;
+CREATE EXTENSION IF NOT EXISTS pgtap;
+RESET client_min_messages;
+
+SET search_path TO beeeon, public;
+
+BEGIN;
+
+SELECT plan(2);
+
+SELECT has_function('device_properties_by_device');
+
+SELECT is_empty(
+	$$ SELECT * FROM device_properties_by_device(11678152912333531136, 1240795450208837) $$,
+	'there is nothing yet in the device_properties table'
+);
+
+SELECT finish();
+ROLLBACK;

commit 9ef60ae43a48edecf6e85287afa57dcbb771ac33

beeeon: fix missing to_device_id() for device_properties_by_device
Fixes: 0873ff60d3f4 ("beeeon: implement device_properties_by_device")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

deploy/func/device_properties_by_device.sql
@@ -18,7 +18,7 @@ BEGIN
 			params
 		FROM beeeon.device_properties
 		WHERE
-			device_id = _device_id
+			device_id = beeeon.to_device_id(_device_id)
 			AND
 			gateway_id = _gateway_id;
 END;
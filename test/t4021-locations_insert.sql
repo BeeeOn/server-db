1a47d7861aee0c7b7d12e140bfc4870cad1300df
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-01-20 23:56:02 +0100

    api: use direct query instead of function locations_insert
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- test/t4021-locations_insert.sql
+++ test/t4021-locations_insert.sql
@@ -4,14 +4,17 @@ RESET client_min_messages;
 
 SET search_path TO beeeon, public;
 
+\set query `cat _api/locations.create.sql`
+
 BEGIN;
 
-SELECT plan(5);
+PREPARE locations_insert(varchar, varchar, bigint)
+AS :query;
 
-SELECT has_function('locations_insert');
+SELECT plan(4);
 
 SELECT throws_ok(
-	$$ SELECT locations_insert('36dd0e4c-e049-44e1-be03-0c7bbca705f2', 'Living room', 1509106553732838) $$,
+	$$ EXECUTE locations_insert('36dd0e4c-e049-44e1-be03-0c7bbca705f2', 'Living room', 1509106553732838) $$,
 	23503,
 	NULL,
 	'cannot insert location associated with a non-existing gateway'
@@ -21,7 +24,7 @@ INSERT INTO gateways (id, name, altitude, latitude, longitude)
 VALUES (1509106553732838, 'MyHome', 0, 0.0, 0.0);
 
 SELECT lives_ok(
-	$$ SELECT locations_insert('36dd0e4c-e049-44e1-be03-0c7bbca705f2', 'Living room', 1509106553732838) $$,
+	$$ EXECUTE locations_insert('36dd0e4c-e049-44e1-be03-0c7bbca705f2', 'Living room', 1509106553732838) $$,
 	'location should be inserted associated with the existing gateway 1509106553732838'
 );
 
@@ -38,7 +41,7 @@ SELECT ok(EXISTS(
 );
 
 SELECT throws_ok(
-	$$ SELECT locations_insert('36dd0e4c-e049-44e1-be03-0c7bbca705f2', 'Living room', 1509106553732838) $$,
+	$$ EXECUTE locations_insert('36dd0e4c-e049-44e1-be03-0c7bbca705f2', 'Living room', 1509106553732838) $$,
 	23505,
 	NULL,
 	'primary key violation when creating the same location twice'

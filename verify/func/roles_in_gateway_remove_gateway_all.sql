7290a6b443d3724a243b9b60e86913f533349ab6
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-01-20 23:56:02 +0100

    api: use direct query instead of function roles_in_gateway_remove_gateway_all
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- verify/func/roles_in_gateway_remove_gateway_all.sql
+++ verify/func/roles_in_gateway_remove_gateway_all.sql
@@ -2,9 +2,8 @@
 
 BEGIN;
 
-SELECT beeeon.assure_function(
-	'beeeon',
-	'roles_in_gateway_remove_gateway_all(bigint)'
-);
+SELECT 0 / CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END
+	FROM pg_proc JOIN pg_namespace ON pronamespace = pg_namespace.oid
+	WHERE proname = 'roles_in_gateway_remove_gateway_all' AND nspname = 'beeeon';
 
 ROLLBACK;

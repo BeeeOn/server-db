3cb10981867147988ee35cc6331ee7fb65b87153
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-01-20 23:56:02 +0100

    api: use direct query instead of function gateways_accessible
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- verify/func/gateways_status_insert_or_skip.sql
+++ verify/func/gateways_status_insert_or_skip.sql
@@ -2,9 +2,8 @@
 
 BEGIN;
 
-SELECT beeeon.assure_function(
-	'beeeon',
-	'gateways_status_insert_or_skip(bigint, bigint, varchar(40), varchar(45))'
-);
+SELECT 0 / CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END
+	FROM pg_proc JOIN pg_namespace ON pronamespace = pg_namespace.oid
+	WHERE proname = 'gateways_status_insert_or_skip' AND nspname = 'beeeon';
 
 ROLLBACK;

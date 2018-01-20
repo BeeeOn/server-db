d55b918dda24c0497ce9d1ab07200ace6169da4a
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-01-20 23:56:02 +0100

    api: use direct query instead of function sensor_history_recent_insert
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- verify/func/sensor_history_recent_insert.sql
+++ verify/func/sensor_history_recent_insert.sql
@@ -2,9 +2,8 @@
 
 BEGIN;
 
-SELECT beeeon.assure_function(
-	'beeeon',
-	'sensor_history_recent_insert(bigint, bigint, smallint, timestamp, real)'
-);
+SELECT 0 / CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END
+	FROM pg_proc JOIN pg_namespace ON pronamespace = pg_namespace.oid
+	WHERE proname = 'sensor_history_recent_insert' AND nspname = 'beeeon';
 
 ROLLBACK;

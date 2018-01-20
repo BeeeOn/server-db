39d79241420c0319e5633d97e2acd59a2cd3cd4c
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-01-20 23:56:02 +0100

    api: use direct query instead of function legacy_roles_in_gateway_by_id
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- verify/func/legacy_roles_in_gateway_by_id.sql
+++ verify/func/legacy_roles_in_gateway_by_id.sql
@@ -2,9 +2,8 @@
 
 BEGIN;
 
-SELECT beeeon.assure_function(
-	'beeeon',
-	'legacy_roles_in_gateway_by_id(uuid)'
-);
+SELECT 0 / CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END
+	FROM pg_proc JOIN pg_namespace ON pronamespace = pg_namespace.oid
+	WHERE proname = 'legacy_roles_in_gateway_by_id' AND nspname = 'beeeon';
 
 ROLLBACK;

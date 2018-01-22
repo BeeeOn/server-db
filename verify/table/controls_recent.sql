faed8966bc88077dbd58d8cf6e2e088cf7c72d65
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-01-22 15:00:17 +0100

    beeeon: drop table controls_recent
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- verify/table/controls_recent.sql
+++ verify/table/controls_recent.sql
@@ -2,19 +2,8 @@
 
 BEGIN;
 
-SELECT
-	gateway_id,
-	device_id,
-	module_id,
-	value,
-	at,
-	stability,
-	originator_user_id
-FROM beeeon.controls_recent WHERE FALSE;
-
-SELECT beeeon.assure_table_priviledges(
-	'beeeon_user', 'beeeon.controls_recent',
-	ARRAY['select', 'insert', 'update', 'delete']
-);
+SELECT 0 / CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END
+	FROM pg_tables
+	WHERE schemaname = 'beeeon' AND tablename = 'controls_recent';
 
 ROLLBACK;

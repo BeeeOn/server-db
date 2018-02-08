BeeeOn/server-db

commit d61d844c7b5563e4a3f0b25e38f91b7fff727c76

beeeon: implement roles_in_gateway_by_id

sqitch.plan
@@ -51,3 +51,4 @@ func/verified_identities_update [schema-beeeon table/verified_identities] 2017-0
 func/verified_identities_remove [schema-beeeon table/verified_identities] 2017-07-14T16:30:59Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement verified_identities_remove
 func/roles_in_gateway_insert [schema-beeeon table/roles_in_gateway] 2017-07-11T14:40:48Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement roles_in_gateway_insert
 func/roles_in_gateway_update [schema-beeeon table/roles_in_gateway] 2017-07-11T15:48:55Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement roles_in_gateway_update
+func/roles_in_gateway_by_id [schema-beeeon type/role_in_gateway table/roles_in_gateway table/identities] 2017-07-11T16:02:04Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement roles_in_gateway_by_id

verify/func/roles_in_gateway_by_id.sql
@@ -1,0 +1,10 @@
+-- beeeon-server pg
+
+BEGIN;
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'roles_in_gateway_by_id(uuid)'
+);
+
+ROLLBACK;

test/t4063-roles_in_gateway_by_id.sql
@@ -1,0 +1,19 @@
+SET client_min_messages TO warning;
+CREATE EXTENSION IF NOT EXISTS pgtap;
+RESET client_min_messages;
+
+SET search_path TO beeeon, public;
+
+BEGIN;
+
+SELECT plan(2);
+
+SELECT has_function('roles_in_gateway_by_id');
+
+SELECT is_empty(
+	$$ SELECT * FROM roles_in_gateway_by_id('b6c168de-32c0-45a9-b23e-c936ac217ef1') $$,
+	'there is nothing yet in the roles_in_gateway table'
+);
+
+SELECT finish();
+ROLLBACK;

deploy/func/roles_in_gateway_by_id.sql
@@ -1,0 +1,26 @@
+-- beeeon-server pg
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION beeeon.roles_in_gateway_by_id(
+	_id uuid
+)
+RETURNS SETOF beeeon.role_in_gateway AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT
+			r.id,
+			r.gateway_id,
+			i.id,
+			r.level,
+			extract(epoch FROM r.created)::bigint,
+			i.email
+		FROM beeeon.roles_in_gateway AS r
+		JOIN beeeon.identities AS i
+			ON r.identity_id = i.id
+		LIMIT 1;
+END;
+$$ LANGUAGE plpgsql;
+
+COMMIT;

revert/func/roles_in_gateway_by_id.sql
@@ -1,0 +1,7 @@
+-- beeeon-server pg
+
+BEGIN;
+
+DROP FUNCTION beeeon.roles_in_gateway_by_id(uuid);
+
+COMMIT;

commit 5c49ac422c0efac9c74dfd6781f34e7a249f050f

api: use direct query instead of function roles_in_gateway_by_id
This change also fixes a bug in the original query where is missing
the WHERE clause and the argument _id is thus never used.

Fixes: d61d844c7b55 ("beeeon: implement roles_in_gateway_by_id")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

server-queries.ini
@@ -37,7 +37,7 @@ verified_identities.remove         = DELETE FROM beeeon.verified_identities WHER
 
 roles_in_gateway.create              = INSERT INTO beeeon.roles_in_gateway (id, gateway_id, identity_id, level, created) VALUES ($1, $2, $3, $4, beeeon.as_utc_timestamp($5))
 roles_in_gateway.update              = UPDATE beeeon.roles_in_gateway SET level = $2 WHERE id = $1
-roles_in_gateway.fetch.by.id         = SELECT * FROM beeeon.roles_in_gateway_by_id($1::uuid)
+roles_in_gateway.fetch.by.id         = SELECT r.id AS id, r.gateway_id AS gateway_id, i.id AS identity_id, r.level AS level, extract(epoch FROM r.created)::bigint AS created, i.email AS identity_email FROM beeeon.roles_in_gateway AS r JOIN beeeon.identities AS i ON r.identity_id = i.id WHERE r.id = $1 LIMIT 1
 roles_in_gateway.fetch.by.gateway_id = SELECT * FROM beeeon.roles_in_gateway_by_gateway($1::bigint)
 roles_in_gateway.remove              = SELECT beeeon.roles_in_gateway_remove($1::uuid)
 roles_in_gateway.remove.user         = SELECT beeeon.roles_in_gateway_remove_user($1::uuid, $2::bigint)

sqitch.plan
@@ -145,3 +145,4 @@ func/device_properties_by_key [func/device_properties_by_key@prealpha] 2018-02-0
 func/device_properties_by_device [func/device_properties_by_device@prealpha] 2018-02-06T15:01:42Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: remove function device_properties_by_device
 func/roles_in_gateway_insert [func/roles_in_gateway_insert@prealpha] 2018-02-07T15:03:19Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: remove function roles_in_gateway_insert
 func/roles_in_gateway_update [func/roles_in_gateway_update@prealpha] 2018-02-07T15:08:06Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: remove function roles_in_gateway_update
+func/roles_in_gateway_by_id [func/roles_in_gateway_by_id@prealpha] 2018-02-08T12:37:40Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: remove function roles_in_gateway_by_id

verify/func/roles_in_gateway_by_id.sql
@@ -2,9 +2,8 @@
 
 BEGIN;
 
-SELECT beeeon.assure_function(
-	'beeeon',
-	'roles_in_gateway_by_id(uuid)'
-);
+SELECT 0 / CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END
+	FROM pg_proc JOIN pg_namespace ON pronamespace = pg_namespace.oid
+	WHERE proname = 'roles_in_gateway_by_id' AND nspname = 'beeeon';
 
 ROLLBACK;

verify/func/roles_in_gateway_by_id@prealpha.sql
@@ -1,0 +1,10 @@
+-- beeeon-server pg
+
+BEGIN;
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'roles_in_gateway_by_id(uuid)'
+);
+
+ROLLBACK;

test/t4063-roles_in_gateway_by_id.sql
@@ -4,11 +4,14 @@ RESET client_min_messages;
 
 SET search_path TO beeeon, public;
 
+\set query $$ `cat _api/roles_in_gateway.fetch.by.id.sql`; $$
+
 BEGIN;
 
-SELECT plan(2);
+CREATE OR REPLACE FUNCTION roles_in_gateway_by_id(uuid)
+RETURNS SETOF beeeon.role_in_gateway AS :query LANGUAGE SQL;
 
-SELECT has_function('roles_in_gateway_by_id');
+SELECT plan(1);
 
 SELECT is_empty(
 	$$ SELECT * FROM roles_in_gateway_by_id('b6c168de-32c0-45a9-b23e-c936ac217ef1') $$,
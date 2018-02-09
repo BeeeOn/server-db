9d16dcec7152e2bc5f9f820d3c3c2a874645e560
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-02-09 17:48:42 +0100

    as_utc_timestamp_us: fix for pgsql older than 9.4
    The PostgreSQL prior to 9.4 does not contain function make_interval.
    The function was never called during testing before and thus has
    been discovered now.
    
    Fixes: 87de1b5ce44a ("beeeon: introduce function as_utc_timestamp_us()")
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- deploy/as_utc_timestamp_us.sql
+++ deploy/as_utc_timestamp_us.sql
@@ -6,13 +6,13 @@ CREATE OR REPLACE FUNCTION beeeon.as_utc_timestamp_us(unix bigint)
 RETURNS TIMESTAMP AS
 $$
 DECLARE
-	frac_sec double precision;
+	us double precision;
 BEGIN
 	-- extract fractional parts of seconds (i.e. ms and us)
-	frac_sec := (unix % 1000000)::double precision / 1000000;
+	us := (unix % 1000000)::double precision;
 
 	RETURN beeeon.as_utc_timestamp(unix / 1000000)
-		+ make_interval(0, 0, 0, 0, 0, 0, frac_sec);
+		+ us * interval '1 microseconds';
 END;
 $$ LANGUAGE plpgsql;
 
---
+++ verify/as_utc_timestamp_us@prealpha.sql
@@ -0,0 +1,10 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT has_function_privilege(
+	'beeeon.as_utc_timestamp_us(bigint)',
+	'execute'
+);
+
+ROLLBACK;

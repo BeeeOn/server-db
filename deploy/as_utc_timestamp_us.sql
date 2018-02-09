BeeeOn/server-db

commit 87de1b5ce44a3f852504b8fa5c542a4c6627b37e

beeeon: introduce function as_utc_timestamp_us()
We may need to accept timestamps in microseconds or milliseconds. While
the current function as_utc_timestamp() accepts seconds.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

deploy/as_utc_timestamp_us.sql
@@ -1,0 +1,19 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION beeeon.as_utc_timestamp_us(unix bigint)
+RETURNS TIMESTAMP AS
+$$
+DECLARE
+	frac_sec double precision;
+BEGIN
+	-- extract fractional parts of seconds (i.e. ms and us)
+	frac_sec := (unix % 1000000)::double precision / 1000000;
+
+	RETURN beeeon.as_utc_timestamp(unix / 1000000)
+		+ make_interval(0, 0, 0, 0, 0, 0, frac_sec);
+END;
+$$ LANGUAGE plpgsql;
+
+COMMIT;

sqitch.plan
@@ -94,3 +94,4 @@ func/controls_recent_state [schema-beeeon type/control_stability table/controls_
 type/fcm_token [schema-beeeon] 2017-09-26T14:39:25Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: new type fcm_token
 func/fcm_tokens_by_id [schema-beeeon type/fcm_token table/users table/fcm_tokens] 2017-09-01T16:22:42Z Peter Kubov <xkubov06@stud.fit.vutbr.cz> # beeeon: implements fcm_tokens_by_id
 func/fcm_recipients_by_gateway [schema-beeeon type/fcm_token table/users table/verified_identities table/roles_in_gateway table/fcm_tokens] 2017-11-29T16:53:29Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: implement fcm_recipients_by_gateway
+as_utc_timestamp_us [schema-beeeon] 2017-12-06T15:30:47Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: modify introduce function as_utc_timestamp_us()

verify/as_utc_timestamp_us.sql
@@ -1,0 +1,10 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT has_function_privilege(
+	'beeeon.as_utc_timestamp_us(bigint)',
+	'execute'
+);
+
+ROLLBACK;

revert/as_utc_timestamp_us.sql
@@ -1,0 +1,7 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+DROP FUNCTION beeeon.as_utc_timestamp_us(bigint);
+
+COMMIT;

commit 9d16dcec7152e2bc5f9f820d3c3c2a874645e560

as_utc_timestamp_us: fix for pgsql older than 9.4
The PostgreSQL prior to 9.4 does not contain function make_interval.
The function was never called during testing before and thus has
been discovered now.

Fixes: 87de1b5ce44a ("beeeon: introduce function as_utc_timestamp_us()")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

deploy/as_utc_timestamp_us.sql
@@ -6,13 +6,13 @@ CREATE OR REPLACE FUNCTION beeeon.as_utc_timestamp_us(unix bigint)
 RETURNS TIMESTAMP AS
 $$
 DECLARE
-	frac_sec double precision;
+	us double precision;
 BEGIN
 	-- extract fractional parts of seconds (i.e. ms and us)
-	frac_sec := (unix % 1000000)::double precision / 1000000;
+	us := (unix % 1000000)::double precision;
 
 	RETURN beeeon.as_utc_timestamp(unix / 1000000)
-		+ make_interval(0, 0, 0, 0, 0, 0, frac_sec);
+		+ us * interval '1 microseconds';
 END;
 $$ LANGUAGE plpgsql;
 

revert/as_utc_timestamp_us@prealpha.sql
@@ -1,0 +1,7 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+DROP FUNCTION beeeon.as_utc_timestamp_us(bigint);
+
+COMMIT;

sqitch.plan
@@ -159,3 +159,4 @@ func/roles_in_gateway_access_level [func/roles_in_gateway_access_level@prealpha]
 func/roles_in_gateway_accessible_gateways [func/roles_in_gateway_accessible_gateways@prealpha] 2018-02-08T15:36:59Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: remove function roles_in_gateway_accessible_gateways
 func/legacy_roles_in_gateway_by_id [func/legacy_roles_in_gateway_by_id@prealpha] 2018-02-08T15:56:57Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: remove function legacy_roles_in_gateway_by_id
 func/legacy_roles_in_gateway_by_gateway [func/legacy_roles_in_gateway_by_gateway@prealpha] 2018-02-08T12:53:14Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon: remove function legacy_roles_in_gateway_by_gateway
+as_utc_timestamp_us [as_utc_timestamp_us@prealpha] 2018-02-09T16:42:09Z Jan Viktorin <iviktorin@fit.vutbr.cz> # fix as_utc_timestamp_us to work with pgsql older than 9.4

verify/as_utc_timestamp_us.sql
@@ -7,4 +7,14 @@ SELECT has_function_privilege(
 	'execute'
 );
 
+SELECT 1 / COUNT(*)
+	FROM pg_proc
+	JOIN pg_namespace ON pronamespace = pg_namespace.oid
+	WHERE
+		proname = 'as_utc_timestamp_us'
+		AND
+		nspname = 'beeeon'
+		AND
+		pg_get_functiondef(pg_proc.oid) NOT LIKE $$%make_interval%$$;
+
 ROLLBACK;

verify/as_utc_timestamp_us@prealpha.sql
@@ -1,0 +1,10 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT has_function_privilege(
+	'beeeon.as_utc_timestamp_us(bigint)',
+	'execute'
+);
+
+ROLLBACK;
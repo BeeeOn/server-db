dfccb6765e298a804955c16e77794c424fcd1283
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-02-13 16:38:50 +0100

    beeeon: remove type role_in_gateway
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- verify/type/role_in_gateway.sql
+++ verify/type/role_in_gateway.sql
@@ -2,6 +2,11 @@
 
 BEGIN;
 
-SELECT beeeon.assure_type('beeeon', 'role_in_gateway');
+SELECT 1 / CASE WHEN COUNT(*) > 0 THEN 0 ELSE 1 END
+	FROM pg_catalog.pg_type t
+	JOIN pg_catalog.pg_namespace n ON t.typnamespace = n.oid
+	WHERE t.typisdefined
+		AND n.nspname = 'beeeon'
+		AND t.typname = 'role_in_gateway';
 
 ROLLBACK;

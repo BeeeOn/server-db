deb0eb3c03a1b2598ca36c0562a2fff636bbc9b8
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-02-14 23:51:32 +0100

    beeeon.functions: create device_id utilities
    Provide utility methods for converting data types to the internal
    device ID representation - bigint. The bigint is 64-bit wide but
    it is signed while the device ID is assumed to be unsigned. The
    conversion methods help to preserve the device ID properties while
    storing it as bigint.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ revert/beeeon.functions/create_device_id_utilities.sql
@@ -0,0 +1,10 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+DROP FUNCTION beeeon.from_device_id(bigint);
+DROP FUNCTION beeeon.to_device_id(bigint);
+DROP FUNCTION beeeon.to_device_id(varchar);
+DROP FUNCTION beeeon.to_device_id(numeric);
+
+COMMIT;

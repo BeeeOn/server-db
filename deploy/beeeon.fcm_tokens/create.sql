0166a1f961a26a39209db7edbff32cb7e0be4b72
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-02-14 22:12:49 +0100

    beeeon.fcm_tokens: create new table
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

---
+++ deploy/beeeon.fcm_tokens/create.sql
@@ -0,0 +1,18 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SET client_min_messages = 'warning';
+
+CREATE TABLE beeeon.fcm_tokens (
+	token varchar(250) NOT NULL,
+	user_id uuid NOT NULL,
+	CONSTRAINT fcm_tokens_pk PRIMARY KEY (token),
+	CONSTRAINT fcm_tokens_fk FOREIGN KEY (user_id) REFERENCES beeeon.users (id)
+);
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+	ON TABLE beeeon.fcm_tokens
+	TO beeeon_user;
+
+COMMIT;

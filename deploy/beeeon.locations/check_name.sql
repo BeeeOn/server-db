fef88c67e4584b548552c520c2f0d43b83425fd7
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-04-18 14:41:07 +0200

    check_name: all check_name constraints allows symbol +
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- deploy/beeeon.locations/check_name.sql
+++ deploy/beeeon.locations/check_name.sql
@@ -4,6 +4,6 @@ BEGIN;
 
 ALTER TABLE beeeon.locations
 	ADD CONSTRAINT check_name_valid
-	CHECK (name ~ '^[[:alnum:] \.:!?()/,\-_#''$€¥£©®]*$');
+	CHECK (name ~ '^[[:alnum:] \.:!?()/,\-_#''$€¥£©®+]*$');
 
 COMMIT;
--- deploy/beeeon.users/check_first_and_last_name.sql
+++ deploy/beeeon.users/check_first_and_last_name.sql
@@ -5,8 +5,8 @@ BEGIN;
 ALTER TABLE beeeon.users
 	ADD CONSTRAINT check_first_and_last_name_valid
 	CHECK (
-		first_name ~ '^[[:alnum:] \.:!?()/,\-_#''$€¥£©®]*$'
+		first_name ~ '^[[:alnum:] \.:!?()/,\-_#''$€¥£©®+]*$'
 		AND
-		last_name ~ '^[[:alnum:] \.:!?()/,\-_#''$€¥£©®]*$');
+		last_name ~ '^[[:alnum:] \.:!?()/,\-_#''$€¥£©®+]*$');
 
 COMMIT;

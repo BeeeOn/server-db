d34654cbf30eaf30d598b96dd284174ecd813127
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-04-23 19:40:56 +0200

    api: optimize sensors_history.huge_fetch_agg
    Get rid of the CTE that is really unneeded in this query. Its original
    purpose was to avoid ODBC to fail because we ware referencing the $6
    parameter multiple times. This is no longer true and we can avoid the
    CTE (probably unneeded I/O).
    
    The type of $6 is also changed to integer as the bigint is simply too
    big for this purpose.
    
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- pgsql/sensors_history/huge_fetch_agg.sql
+++ pgsql/sensors_history/huge_fetch_agg.sql
@@ -1,40 +1,29 @@
-WITH
-input AS (
-	SELECT
-		$1::bigint AS gateway_id,
-		$2::numeric(20, 0) AS device_id,
-		$3::smallint AS module_id,
-		beeeon.as_utc_timestamp($4::bigint) AS start_ts,
-		beeeon.as_utc_timestamp($5::bigint) AS end_ts,
-		$6::bigint AS secs_interval
-)
 SELECT
 	extract(epoch FROM
 		r.at - interval '1 second' * (
 			(60 * date_part('minute', r.at)::bigint
 				+ date_part('second', r.at)::bigint)
-			% input.secs_interval)
+			% $6::integer)
 	)::bigint AS at,
 	AVG(r.value)::real AS avg,
 	MIN(r.value)::real AS min,
 	MAX(r.value)::real AS max
 FROM
-	beeeon.sensor_history_raw AS r,
-	input
+	beeeon.sensor_history_raw AS r
 WHERE
 	r.refid = (SELECT refid
 		FROM beeeon.sensors
 		WHERE
-			gateway_id = input.gateway_id
+			gateway_id = $1::bigint
 			AND
-			device_id = beeeon.to_device_id(input.device_id)
+			device_id = beeeon.to_device_id($2::numeric(20, 0))
 			AND
-			module_id = input.module_id
+			module_id = $3::smallint
 		LIMIT 1)
 	AND
-	input.start_ts <= r.at
+	beeeon.as_utc_timestamp($4::bigint) <= r.at
 	AND
-	r.at < input.end_ts
+	r.at < beeeon.as_utc_timestamp($5::bigint)
 	AND
 	r.value IS NOT NULL
 GROUP BY 1

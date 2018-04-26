BeeeOn/server-db

commit d8d57b54f0bb389d60b1a9c0f788d6b984a88108

beeeon.controls_fsm_last: create new materialized view
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

sqitch.plan
@@ -16,3 +16,4 @@ beeeon.roles_in_gateway/create 2018-02-14T20:27:11Z Jan Viktorin <iviktorin@fit.
 beeeon.sensor_history_recent/create 2018-02-14T20:30:19Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.sensor_history_recent: create new table
 beeeon.sensor_history_last/create 2018-02-14T20:33:04Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.sensor_history_last: create new materialized view
 beeeon.controls_fsm/create 2018-02-14T20:39:05Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.controls_fsm: create new table
+beeeon.controls_fsm_last/create 2018-02-14T20:52:59Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.controls_fsm_last: create new materialized view

deploy/beeeon.controls_fsm_last/create.sql
@@ -1,0 +1,214 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SET client_min_messages = 'warning';
+
+---
+-- The table works as a materialized view of the controls_fsm
+-- table and stores only the most recent transition for each
+-- control.
+---
+CREATE TABLE beeeon.controls_fsm_last AS
+WITH newset_values AS (
+	SELECT
+		gateway_id,
+		device_id,
+		module_id,
+		MAX(requested_at) AS requested_at
+	FROM beeeon.controls_fsm
+	GROUP BY gateway_id, device_id, module_id
+)
+SELECT
+	c.gateway_id AS gateway_id,
+	c.device_id AS device_id,
+	c.module_id AS module_id,
+	c.value AS value,
+	c.requested_at AS requested_at,
+	c.accepted_at AS accepted_at,
+	c.finished_at AS finished_at,
+	c.failed AS failed,
+	c.originator_user_id AS originator_user_id
+FROM beeeon.controls_fsm AS c
+JOIN newset_values AS n ON
+	c.gateway_id = n.gateway_id
+	AND
+	c.device_id = n.device_id
+	AND
+	c.module_id = n.module_id
+	AND
+	c.requested_at = n.requested_at;
+
+---
+-- Apply constraints to the table controls_fsm_last.
+---
+
+ALTER TABLE beeeon.controls_fsm_last
+	ADD CONSTRAINT controls_fsm_last_pk
+	PRIMARY KEY (gateway_id, device_id, module_id);
+
+ALTER TABLE beeeon.controls_fsm_last
+	ADD CONSTRAINT controls_fsm_last_fk
+	FOREIGN KEY (gateway_id, device_id, module_id, requested_at)
+	REFERENCES beeeon.controls_fsm (gateway_id, device_id, module_id, requested_at);
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+	ON TABLE beeeon.controls_fsm_last
+	TO beeeon_user;
+
+---
+-- Logic that updates the table controls_fsm_last after insert
+-- into the table controls_fsm.
+---
+CREATE OR REPLACE FUNCTION beeeon.controls_fsm_last_after_insert()
+RETURNS TRIGGER AS
+$$
+DECLARE
+	_requested_at timestamp;
+	_finished_at timestamp;
+	_value real;
+	_originator_user_id uuid;
+BEGIN
+	SELECT
+		requested_at,
+		accepted_at,
+		finished_at,
+		value,
+		originator_user_id
+	INTO
+		_requested_at,
+		_finished_at,
+		_value,
+		_originator_user_id
+	FROM beeeon.controls_fsm_last
+	WHERE
+		gateway_id = NEW.gateway_id
+		AND
+		device_id = NEW.device_id
+		AND
+		module_id = NEW.module_id;
+
+	IF NOT FOUND THEN
+		INSERT INTO beeeon.controls_fsm_last (
+			gateway_id,
+			device_id,
+			module_id,
+			value,
+			requested_at,
+			accepted_at,
+			finished_at,
+			failed,
+			originator_user_id
+		)
+		VALUES (
+			NEW.gateway_id,
+			NEW.device_id,
+			NEW.module_id,
+			NEW.value,
+			NEW.requested_at,
+			NEW.accepted_at,
+			NEW.finished_at,
+			NEW.failed,
+			NEW.originator_user_id
+		);
+
+	ELSIF _requested_at < NEW.requested_at THEN
+		IF _finished_at IS NULL THEN
+			RAISE EXCEPTION
+				'could not begin a new request (%) while'
+				' another (%) is in progress',
+				NEW.requested_at, _requested_at;
+		END IF;
+
+		UPDATE beeeon.controls_fsm_last
+		SET
+			value = NEW.value,
+			requested_at = NEW.requested_at,
+			accepted_at = NEW.accepted_at,
+			finished_at = NEW.finished_at,
+			failed = NEW.failed,
+			originator_user_id = NEW.originator_user_id
+		WHERE
+			gateway_id = NEW.gateway_id
+			AND
+			device_id = NEW.device_id
+			AND
+			module_id = NEW.module_id;
+	END IF;	
+
+	RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER control_fsm_last_insert_trigger
+AFTER INSERT ON beeeon.controls_fsm
+FOR EACH ROW
+	EXECUTE PROCEDURE beeeon.controls_fsm_last_after_insert();
+
+---
+-- Logic that updates the table controls_fsm_last after update
+-- of the table controls_fsm.
+---
+CREATE OR REPLACE FUNCTION beeeon.controls_fsm_last_after_update()
+RETURNS TRIGGER AS
+$$
+DECLARE
+	_requested_at timestamp;
+	_value real;
+	_originator_user_id uuid;
+BEGIN
+	SELECT
+		requested_at,
+		accepted_at,
+		finished_at,
+		value,
+		originator_user_id
+	INTO
+		_requested_at,
+		_value,
+		_originator_user_id
+	FROM beeeon.controls_fsm_last
+	WHERE
+		gateway_id = NEW.gateway_id
+		AND
+		device_id = NEW.device_id
+		AND
+		module_id = NEW.module_id;
+
+	-- this should happen on update
+	IF FOUND AND _requested_at = NEW.requested_at THEN
+		IF _value <> NEW.value THEN
+			RAISE EXCEPTION
+				'value cannot be changed from % to %',
+				_value, NEW.value;
+		END IF;
+
+		IF _originator_user_id <> NEW.originator_user_id THEN
+			RAISE EXCEPTION
+				'originator cannot be changed from % to %',
+				_originator_user_id, NEW.originator_user_id;
+		END IF;
+
+		UPDATE beeeon.controls_fsm_last
+		SET
+			accepted_at = NEW.accepted_at,
+			finished_at = NEW.finished_at,
+			failed = NEW.failed
+		WHERE
+			gateway_id = NEW.gateway_id
+			AND
+			device_id = NEW.device_id
+			AND
+			module_id = NEW.module_id;
+	END IF;	
+
+	RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER control_fsm_last_update_trigger
+AFTER UPDATE ON beeeon.controls_fsm
+FOR EACH ROW
+	EXECUTE PROCEDURE beeeon.controls_fsm_last_after_update();
+
+COMMIT;

revert/beeeon.controls_fsm_last/create.sql
@@ -1,0 +1,15 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+DROP TRIGGER control_fsm_last_update_trigger
+	ON beeeon.controls_fsm;
+DROP FUNCTION beeeon.controls_fsm_last_after_update();
+
+DROP TRIGGER control_fsm_last_insert_trigger
+	ON beeeon.controls_fsm;
+DROP FUNCTION beeeon.controls_fsm_last_after_insert();
+
+DROP TABLE beeeon.controls_fsm_last;
+
+COMMIT;

verify/beeeon.controls_fsm_last/create.sql
@@ -1,0 +1,32 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT
+	gateway_id,
+	device_id,
+	module_id,
+	value,
+	requested_at,
+	accepted_at,
+	finished_at,
+	failed,
+	originator_user_id
+FROM beeeon.controls_fsm_last WHERE FALSE;
+
+SELECT beeeon.assure_table_priviledges(
+	'beeeon_user', 'beeeon.controls_fsm_last',
+	ARRAY['select', 'insert', 'update', 'delete']
+);
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'controls_fsm_last_after_insert()'
+);
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'controls_fsm_last_after_update()'
+);
+
+ROLLBACK;

commit b82b1aebd61da3e04fe079fe9f392cf9aeb6b4b7

beeeon.controls_fsm_last: fix fsm_last_after_insert
Fix trigger function fsm_last_after_insert that might fail
during execution due to unmatching columns in SELECT with its
INTO clause.

Fixes: d8d57b54f0bb ("beeeon.controls_fsm_last: create new materialized view")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

deploy/beeeon.controls_fsm_last/fix_fsm_last_after_insert.sql
@@ -1,0 +1,83 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+---
+-- Removed redundant accepted_at, value and originator_user_id from the
+-- first SELECT. The accepted_at could cause errors because the INTO field
+-- did not match the SELECT columns. The others were redundant.
+---
+CREATE OR REPLACE FUNCTION beeeon.controls_fsm_last_after_insert()
+RETURNS TRIGGER AS
+$$
+DECLARE
+	_requested_at timestamp;
+	_finished_at timestamp;
+BEGIN
+	SELECT
+		requested_at,
+		finished_at
+	INTO
+		_requested_at,
+		_finished_at
+	FROM beeeon.controls_fsm_last
+	WHERE
+		gateway_id = NEW.gateway_id
+		AND
+		device_id = NEW.device_id
+		AND
+		module_id = NEW.module_id;
+
+	IF NOT FOUND THEN
+		INSERT INTO beeeon.controls_fsm_last (
+			gateway_id,
+			device_id,
+			module_id,
+			value,
+			requested_at,
+			accepted_at,
+			finished_at,
+			failed,
+			originator_user_id
+		)
+		VALUES (
+			NEW.gateway_id,
+			NEW.device_id,
+			NEW.module_id,
+			NEW.value,
+			NEW.requested_at,
+			NEW.accepted_at,
+			NEW.finished_at,
+			NEW.failed,
+			NEW.originator_user_id
+		);
+
+	ELSIF _requested_at < NEW.requested_at THEN
+		IF _finished_at IS NULL THEN
+			RAISE EXCEPTION
+				'could not begin a new request (%) while'
+				' another (%) is in progress',
+				NEW.requested_at, _requested_at;
+		END IF;
+
+		UPDATE beeeon.controls_fsm_last
+		SET
+			value = NEW.value,
+			requested_at = NEW.requested_at,
+			accepted_at = NEW.accepted_at,
+			finished_at = NEW.finished_at,
+			failed = NEW.failed,
+			originator_user_id = NEW.originator_user_id
+		WHERE
+			gateway_id = NEW.gateway_id
+			AND
+			device_id = NEW.device_id
+			AND
+			module_id = NEW.module_id;
+	END IF;	
+
+	RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+COMMIT;

revert/beeeon.controls_fsm_last/fix_fsm_last_after_insert.sql
@@ -1,0 +1,90 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+---
+-- Revert back the potentially buggy controls_fsm_last_after_insert
+-- introduced by beeeon.controls_fsm_last/create.
+---
+CREATE OR REPLACE FUNCTION beeeon.controls_fsm_last_after_insert()
+RETURNS TRIGGER AS
+$$
+DECLARE
+	_requested_at timestamp;
+	_finished_at timestamp;
+	_value real;
+	_originator_user_id uuid;
+BEGIN
+	SELECT
+		requested_at,
+		accepted_at,
+		finished_at,
+		value,
+		originator_user_id
+	INTO
+		_requested_at,
+		_finished_at,
+		_value,
+		_originator_user_id
+	FROM beeeon.controls_fsm_last
+	WHERE
+		gateway_id = NEW.gateway_id
+		AND
+		device_id = NEW.device_id
+		AND
+		module_id = NEW.module_id;
+
+	IF NOT FOUND THEN
+		INSERT INTO beeeon.controls_fsm_last (
+			gateway_id,
+			device_id,
+			module_id,
+			value,
+			requested_at,
+			accepted_at,
+			finished_at,
+			failed,
+			originator_user_id
+		)
+		VALUES (
+			NEW.gateway_id,
+			NEW.device_id,
+			NEW.module_id,
+			NEW.value,
+			NEW.requested_at,
+			NEW.accepted_at,
+			NEW.finished_at,
+			NEW.failed,
+			NEW.originator_user_id
+		);
+
+	ELSIF _requested_at < NEW.requested_at THEN
+		IF _finished_at IS NULL THEN
+			RAISE EXCEPTION
+				'could not begin a new request (%) while'
+				' another (%) is in progress',
+				NEW.requested_at, _requested_at;
+		END IF;
+
+		UPDATE beeeon.controls_fsm_last
+		SET
+			value = NEW.value,
+			requested_at = NEW.requested_at,
+			accepted_at = NEW.accepted_at,
+			finished_at = NEW.finished_at,
+			failed = NEW.failed,
+			originator_user_id = NEW.originator_user_id
+		WHERE
+			gateway_id = NEW.gateway_id
+			AND
+			device_id = NEW.device_id
+			AND
+			module_id = NEW.module_id;
+	END IF;	
+
+	RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+
+COMMIT;

sqitch.plan
@@ -33,3 +33,4 @@ beeeon.gateways/check_name 2018-04-16T05:33:50Z viktorin <viktorin@ant-2.fit.vut
 beeeon.devices/check_name 2018-04-16T06:06:13Z viktorin <viktorin@ant-2.fit.vutbr.cz> # beeeon.devices: check name to contain valid characters
 beeeon.users/check_first_and_last_name 2018-04-17T10:35:47Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.users: check first_name, last_name to contain valid characters
 beeeon.locations/check_name 2018-04-17T10:45:30Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.locations: check name to contain valid characters
+beeeon.controls_fsm_last/fix_fsm_last_after_insert 2018-04-24T08:37:49Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.controls_fsm_last: fix fsm_last_after_insert

verify/beeeon.controls_fsm_last/fix_fsm_last_after_insert.sql
@@ -1,0 +1,23 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT 1 / COUNT(*)
+FROM
+	pg_catalog.pg_proc AS p
+JOIN
+	pg_catalog.pg_namespace AS n
+ON
+	p.pronamespace = n.oid
+WHERE
+	p.proname = 'controls_fsm_last_after_insert'
+	AND
+	n.nspname = 'beeeon'
+	AND
+	pg_get_functiondef(p.oid) NOT LIKE $$%\_accepted\_at%$$
+	AND
+	pg_get_functiondef(p.oid) NOT LIKE $$%\_value%$$
+	AND
+	pg_get_functiondef(p.oid) NOT LIKE $$%\_originator\_user\_id%$$;
+
+ROLLBACK;
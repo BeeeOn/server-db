BeeeOn/server-db

commit 3ab3652933edd5dddcbf145a14db4827eb06dd6e

perf: sensor_history.insert
Benchmark isnert of data into the sensor history of the candidate
sensors.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

perf/sensors_history/insert.sql
@@ -1,0 +1,42 @@
+SET client_min_messages TO warning;
+CREATE EXTENSION IF NOT EXISTS pgtap;
+RESET client_min_messages;
+
+SET search_path TO beeeon, public;
+
+\set query `cat pgsql/sensors_history/insert.sql`
+
+BEGIN;
+
+\ir ../_common.sql
+\ir _prepare_sensors.sql
+
+PREPARE query_insert AS :query;
+
+DO LANGUAGE plpgsql
+$$
+DECLARE
+	rec record;
+	q text;
+	t bigint;
+BEGIN
+	FOR rec IN (SELECT * FROM candidate_sensors) LOOP
+		FOR i IN 1..10 LOOP
+			t := extract(epoch from beeeon.now_utc())::bigint;
+			q := format(
+				'EXECUTE query_insert(%s, %s, %s, %s, %s)',
+				rec.gateway_id,
+				beeeon.from_device_id(rec.device_id),
+				rec.module_id,
+				t + i,
+				15.5 + i);
+
+			PERFORM explain_analyze(q);
+		END LOOP;
+	END LOOP;
+END;
+$$;
+
+ROLLBACK;
+
+ROLLBACK;

commit 45a23f1f150697929d13b072a1d1f3778af44b1b

perf: fix redundant ROLLBACK statement
Fixes: 3ab3652933ed ("perf: sensor_history.insert")
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

perf/sensors_history/insert.sql
@@ -38,5 +38,3 @@ END;
 $$;
 
 ROLLBACK;
-
-ROLLBACK;
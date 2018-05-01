4cd98351c64127a89d8465e919a495269089252b
Jan Viktorin <iviktorin@fit.vutbr.cz>
2018-05-01 17:39:34 +0200

    beeeon.sensors: fix priviledge to sequence sensors_refid_seq
    When ALTERing beeeon.sensors table to define column constraints and
    properties, we have to also grant permissions to beeeon_user for the
    sequence that is automatically created for refid of type serial.
    
    Without this, the proper users cannot insert into database.
    
    Fixes: 3b818c5dc7e6 ("beeeon.sensors: create new table"
    Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

--- verify/beeeon.sensors/create.sql
+++ verify/beeeon.sensors/create.sql
@@ -17,6 +17,26 @@ SELECT beeeon.assure_table_priviledges(
 	ARRAY['select', 'insert', 'update', 'delete']
 );
 
+DO LANGUAGE plpgsql
+$$
+DECLARE
+	p text;
+	seq text;
+BEGIN
+	seq := 'beeeon.sensors_refid_seq';
+
+	FOREACH p IN ARRAY ARRAY['select', 'usage'] LOOP
+		IF has_sequence_privilege('beeeon_user', seq, p) THEN
+			CONTINUE;
+		END IF;
+
+		RAISE EXCEPTION
+			'beeeon_user should have % priviledge to %',
+			p, seq;
+	END LOOP;
+END;
+$$;
+
 SELECT beeeon.assure_function(
 	'beeeon',
 	'sensors_init(bigint, bigint, smallint)'

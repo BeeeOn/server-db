BeeeOn/server-db

commit 3b818c5dc7e6a3087c1ad0e5aeff0d2605fdfb8d

beeeon.sensors: create new table
Each sensor has now a record in the sensors table. We did not have
any such storage before. No infrastructure counts on such table
and thus, we have to fill it via trigger.

Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

deploy/beeeon.sensors/create.sql
@@ -1,0 +1,125 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SET client_min_messages = 'warning';
+
+---
+-- Generate sensors for each sensor in sensor_history_raw.
+-- Each sensor has its refid (surrogate key) that would be
+-- used for cheap referencing from very huge tables.
+---
+CREATE TABLE beeeon.sensors AS
+SELECT
+	r.gateway_id AS gateway_id,
+	r.device_id AS device_id,
+	r.module_id AS module_id
+FROM
+	beeeon.sensor_history_raw AS r
+GROUP BY
+	r.gateway_id,
+	r.device_id,
+	r.module_id;
+
+ALTER TABLE beeeon.sensors
+	ALTER COLUMN gateway_id SET NOT NULL,
+	ALTER COLUMN device_id SET NOT NULL,
+	ALTER COLUMN module_id SET NOT NULL,
+	ADD COLUMN refid serial NOT NULL;
+
+---
+-- Create natural constraints of the table:
+--
+-- * PK - gateway_id, device_id, module_id
+-- * FK - linking us with devices
+-- * UNIQUE - refid must be unique
+---
+ALTER TABLE beeeon.sensors
+	ADD CONSTRAINT sensors_pk
+	PRIMARY KEY (gateway_id, device_id, module_id);
+
+ALTER TABLE beeeon.sensors
+	ADD CONSTRAINT sensors_devices_fk
+	FOREIGN KEY (gateway_id, device_id)
+		REFERENCES beeeon.devices (gateway_id, id);
+
+ALTER TABLE beeeon.sensors
+	ADD CONSTRAINT refid_unique
+	UNIQUE (refid);
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+	ON TABLE beeeon.sensors
+	TO beeeon_user;
+
+---
+-- Initialize a single record in the sensors table
+-- if no such exists yet - generic function.
+---
+CREATE OR REPLACE FUNCTION beeeon.sensors_init(
+	_gateway_id bigint,
+	_device_id bigint,
+	_module_id smallint)
+RETURNS integer AS
+$$
+DECLARE
+	_refid integer := 0;
+BEGIN
+	SELECT
+		refid
+	INTO
+		_refid
+	FROM
+		beeeon.sensors
+	WHERE
+		gateway_id = _gateway_id
+		AND
+		device_id = _device_id
+		AND
+		module_id = _module_id
+	LIMIT 1;
+
+	IF NOT FOUND THEN
+		INSERT INTO beeeon.sensors (
+			gateway_id,
+			device_id,
+			module_id
+		)
+		VALUES (
+			_gateway_id,
+			_device_id,
+			_module_id
+		)
+		RETURNING refid INTO _refid;
+	END IF;
+
+	RETURN _refid;
+END;
+$$ LANGUAGE plpgsql;
+
+---
+-- Trigger function to init non-existing sensors. It is going
+-- to be changed by future modifications.
+---
+CREATE OR REPLACE FUNCTION beeeon.sensors_init_trigger()
+RETURNS TRIGGER AS
+$$
+DECLARE
+	_refid integer;
+BEGIN
+	SELECT beeeon.sensors_init(
+		NEW.gateway_id,
+		NEW.device_id,
+		NEW.module_id
+	) INTO _refid;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER sensors_init_before_insert
+	BEFORE INSERT
+	ON beeeon.sensor_history_raw
+	FOR EACH ROW EXECUTE
+		PROCEDURE beeeon.sensors_init_trigger();
+
+COMMIT;

verify/beeeon.sensors/create.sql
@@ -1,0 +1,30 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+SELECT
+	gateway_id,
+	device_id,
+	module_id,
+	refid
+FROM
+	beeeon.sensors
+WHERE
+	FALSE;
+
+SELECT beeeon.assure_table_priviledges(
+	'beeeon_user', 'beeeon.sensors',
+	ARRAY['select', 'insert', 'update', 'delete']
+);
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'sensors_init(bigint, bigint, smallint)'
+);
+
+SELECT beeeon.assure_function(
+	'beeeon',
+	'sensors_init_trigger()'
+);
+
+ROLLBACK;

revert/beeeon.sensors/create.sql
@@ -1,0 +1,14 @@
+-- beeeon-server, pg
+
+BEGIN;
+
+DROP TRIGGER sensors_init_before_insert
+	ON beeeon.sensor_history_raw;
+DROP FUNCTION beeeon.sensors_init_trigger();
+DROP FUNCTION beeeon.sensors_init(
+		bigint,
+		bigint,
+		smallint);
+DROP TABLE beeeon.sensors;
+
+COMMIT;

sqitch.plan
@@ -36,3 +36,4 @@ beeeon.locations/check_name 2018-04-17T10:45:30Z Jan Viktorin <iviktorin@fit.vut
 beeeon.controls_fsm_last/fix_fsm_last_after_insert 2018-04-24T08:37:49Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.controls_fsm_last: fix fsm_last_after_insert
 beeeon.functions/create_align_timestamp 2018-02-26T12:01:49Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.functions: create align_timestamp
 beeeon.sensor_history_raw/create_by_rename 2018-04-19T09:45:18Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.sensor_history_raw: rename from beeeon.sensor_history_recent
+beeeon.sensors/create 2018-04-18T13:59:35Z Jan Viktorin <iviktorin@fit.vutbr.cz> # beeeon.sensors: create new table

commit 4cd98351c64127a89d8465e919a495269089252b

beeeon.sensors: fix priviledge to sequence sensors_refid_seq
When ALTERing beeeon.sensors table to define column constraints and
properties, we have to also grant permissions to beeeon_user for the
sequence that is automatically created for refid of type serial.

Without this, the proper users cannot insert into database.

Fixes: 3b818c5dc7e6 ("beeeon.sensors: create new table"
Signed-off-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

deploy/beeeon.sensors/create.sql
@@ -51,6 +51,10 @@ GRANT SELECT, INSERT, UPDATE, DELETE
 	ON TABLE beeeon.sensors
 	TO beeeon_user;
 
+GRANT USAGE, SELECT
+	ON SEQUENCE beeeon.sensors_refid_seq
+	TO beeeon_user;
+
 ---
 -- Initialize a single record in the sensors table
 -- if no such exists yet - generic function.

verify/beeeon.sensors/create.sql
@@ -17,6 +17,26 @@ SELECT beeeon.assure_table_priviledges(
 	ARRAY['select', 'insert', 'update', 'delete']
 );
 
+DO LANGUAGE plpgsql
+$$
+DECLARE
+	p text;
+	seq text;
+BEGIN
+	seq := 'beeeon.sensors_refid_seq';
+
+	FOREACH p IN ARRAY ARRAY['select', 'usage'] LOOP
+		IF has_sequence_privilege('beeeon_user', seq, p) THEN
+			CONTINUE;
+		END IF;
+
+		RAISE EXCEPTION
+			'beeeon_user should have % priviledge to %',
+			p, seq;
+	END LOOP;
+END;
+$$;
+
 SELECT beeeon.assure_function(
 	'beeeon',
 	'sensors_init(bigint, bigint, smallint)'